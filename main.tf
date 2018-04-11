bec89292184079b504155ffd66fbaa6587d15706
Michael Jacobson <mjacobson@8thlight.com>
2018-04-11 15:06:07 +0100

    •

--- main.tf
+++ main.tf
@@ -49,7 +49,7 @@ resource "aws_ecs_service" "mastermind_service" {
 
   placement_constraints {
     type       = "memberOf"
-    expression = "attribute:ecs.availability-zone in [us-east-1a, us-east-1b, us-east-1c, us-east-1d]"
+    expression = "task:family == mastermind"
   }
 
   load_balancer {
